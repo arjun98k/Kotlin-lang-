acf301ae151118111945347967762764a266bba2
arjun98k <arjunkandekar14@gmail.com>
2024-10-10 10:53:22 +0530

    kotlin basic

---
+++ variable and intro/Dataa_type.kt
@@ -0,0 +1,16 @@
+// fun main(args: Array<String>) {
+   
+//   max(33,36)
+// }
+// fun max (a: Int , b: Int){
+//   if (a < b) {
+//         println("b is the larger number")
+//     } else {
+//         println("a is the smaller number")
+//     }
+// }
+
+fun main() {
+  val num1: Int = 35;
+  println(num1)
+} 
---
+++ variable and intro/Var_1.kt
@@ -0,0 +1,8 @@
+fun  main() {
+  
+
+    println("take user input from user ")
+    val ak = readLine()
+    println("this what you wanted to print $ak")
+
+}
